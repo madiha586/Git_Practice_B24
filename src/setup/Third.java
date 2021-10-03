f90b3529aa8417daa01f8009201818f0a2f8883b
madiha <madiha586@gmail.com>
2021-10-03 11:44:23 -0500

    Created third file

---
+++ src/setup/Third.java
@@ -0,0 +1,9 @@
+package setup;
+
+public class Third {
+    public static void main(String[] args) {
+
+        System.out.println("I want to get succeed");
+        System.out.println("Hello my java world");
+    }
+}
